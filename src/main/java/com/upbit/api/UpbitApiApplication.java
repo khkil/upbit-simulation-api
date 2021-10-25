38a62bd1fe86cb53b92db148de5b5f58a8625e20
khkil <khkil@micehub.com>
2021-10-25 14:54:19 +0900

    first commit 및 jdbc 설정

---
+++ src/main/java/com/upbit/api/UpbitApiApplication.java
@@ -0,0 +1,13 @@
+package com.upbit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpbitApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upbit/api/UpbitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upbit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
